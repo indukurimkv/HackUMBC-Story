fa77f2f7801a4a5b94a9a62499479404dc3c9935
Murali I. <indukurimkv@gmail.com>
2024-09-28 15:40:57 -0400

    Update git ignore with stories ignore

--- backend/app/pathutil.py
+++ backend/app/pathutil.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
 
-def getRelPath(filePath, relpath):
+def getRelPath(filePath, relpath) -> str:
     return Path(filePath).parent.joinpath(relpath).absolute()
 
 
 if __name__ == "__main__":
     print(getRelPath(__file__, "db/db.json"))
+    print(getRelPath(__file__, "stories/db.story"))
+    
